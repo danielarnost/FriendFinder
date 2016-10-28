3486fb85e5893ffea122e35bb64f1810ae521124
danielarnost <danielarnost@utexaas.edu>
2016-10-28 15:13:08 -0500

    survey home friends

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var userArray = [
+	{
+		name: "Iggy Pop",
+		photo: "https://www.morrisonhotelgallery.com/images/medium/Iggy%20Pop-Slouch.jpg",
+		scores: [1, 2, 3, 4, 3, 4, 5, 4, 3, 2]
+	},
+	{
+		name: "Vladamir Putin",
+		photo: "http://i.huffpost.com/gen/1133558/original.jpg",
+		scores: [3, 4, 4, 2, 4, 5, 3, 5, 1, 4]
+	},
+	{
+		name: "Adele",
+		photo: "http://i.dailymail.co.uk/i/pix/2015/10/21/12/1FE7DBDD00000578-3282733-Adele-a-15_1445427876133.jpg.jpg",
+		scores: [5, 1, 5, 3, 4, 1, 3, 2, 4, 1]
+	},
+	{
+		name: "Chelsea Handler",
+		photo: "http://hamptons-magazine.com/get/files/image/migration/content_chelsea1.jpg",
+		scores: [4, 1, 5, 3, 2, 1, 2, 4, 4, 2]
+	},
+	{
+		name: "Bernie Saners",
+		photo: "https://img1.steemit.com/0x0/http://moonbattery.com/graphics/BERNIE-SANDERS.jpg",
+		scores: [3, 4, 1, 4, 5, 3, 1, 4, 1, 5]
+	}
+]
+
+module.exports = userArray;
