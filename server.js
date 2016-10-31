028b636860f06f244550e3b63f1f9087f40947c0
danielarnost <danielarnost@utexaas.edu>
2016-10-31 17:33:56 -0500

    ignore

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var bodyParser = require('body-parser');
 var express    = require('express');
 var path       = require('path');
 
-var app = express();
+var app  = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
